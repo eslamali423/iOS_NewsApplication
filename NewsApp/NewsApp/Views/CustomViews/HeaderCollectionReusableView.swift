b6649461a07050a826a6d6a2fc97f73f505af4e1
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-07-22 21:21:58 +0200

    Create HeaderCollectionViewReusableView

---
+++ NewsApp/NewsApp/Views/CustomViews/HeaderCollectionReusableView.swift
@@ -0,0 +1,63 @@
+//
+//  HeaderCollectionReusableView.swift
+//  NewsApp
+//
+//  Created by Eslam Ali  on 22/07/2022.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+    
+    //MARK:- Vars
+    
+    static let identifier = "HeaderCollectionReusableView"
+    
+    public let titleLabel : UILabel = {
+        let label =  UILabel()
+        
+        label.numberOfLines = 1
+        label.textColor = .label
+        label.textAlignment = .left
+        label.text = "Hottest News"
+        label.backgroundColor = .green
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        return label
+    }()
+    
+//    private let collectionView : UICollectionView = {
+//        // Layout
+//        let layout = UICollectionViewFlowLayout()
+//        layout.scrollDirection = .vertical
+//        //   layout.itemSize = CGSize(width: 220, height: 300)
+//
+//        let collectionView = UICollectionView(frame: .zero,  collectionViewLayout: layout)
+//        collectionView.register(HeadlineCollectionViewCell.self, forCellWithReuseIdentifier: HeadlineCollectionViewCell.identifier)
+//        collectionView.backgroundColor = .red
+//        return collectionView
+//    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemYellow
+//        addSubview(titleLabel)
+//        addSubview(collectionView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+   
+    //MARK:- Layouts / Constraints
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        //let imageSize: CGFloat = bounds.height
+        titleLabel.frame = bounds
+      //  collectionView.frame = CGRect(x: 0, y: titleLabel.frame.height + 10, width: bounds.width - 10, height: bounds.height - 60)
+ 
+    }
+    
+    
+}
